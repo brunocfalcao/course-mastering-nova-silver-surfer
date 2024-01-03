a9a5c7c5e8a42ecb36e257966265e54ffac18eb3
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2024-01-04 00:14:25 +0100

    Fix scrolls

--- resources/views/layouts/prelaunched.blade.php
+++ resources/views/layouts/prelaunched.blade.php
@@ -22,15 +22,7 @@
 </head>
 
 <body class="antialiased max-h-scr">
-
-<button onclick="scrollToTop()" id="scroll-to-top-button" title="Scroll to top"
-        class="fixed bottom-20 right-20 z-99 bg-teal-100/50 text-black p-1 rounded-full">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-         class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"/>
-    </svg>
-</button>
-
+@include('course::partials.scroll-to-top')
 
 @yield('content')
 
@@ -44,7 +36,7 @@
     };
 
     function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        if (document.body.scrollTop > 30 || document.documentElement.scrollTop > (window.innerHeight / 5)) {
             scrollToTopButton.style.display = "block";
         } else {
             scrollToTopButton.style.display = "none";
--- resources/views/prelaunched/footer.blade.php
+++ resources/views/prelaunched/footer.blade.php
@@ -1,17 +1,31 @@
 <div class="w-full my-10 py-10">
     <div class="max-w-7xl mx-auto px-[4.5rem]">
         <div class="w-full flex flex-col-reverse md:flex-row justify-between">
-            <a href="/" class="flex flex-row justify-center md:justify-start items-center">
-                <img class="w-20 h-20" src="{{ Vite::image('mastering-nova-mascot.svg') }}" alt="mastering nova">
-                <span class="leading-normal tracking-tighter  -medium ml-2 text-3xl">
+            <div>
+
+                <a href="/" class="flex flex-row justify-center md:justify-start items-center">
+                    <img class="w-20 h-20" src="{{ Vite::image('mastering-nova-mascot.svg') }}" alt="mastering nova">
+                    <span class="leading-normal tracking-tighter  -medium ml-2 text-3xl">
                     masteringnova
                 </span>
-            </a>
-            <div class="space-x-1 py-4 leadning-normal tracking-tighter text-lg flex flex-col md:flex-row items-center">
+                </a>
+                <div class="w-full flex justify-center">
+                    <button onclick="scrollToTop()"
+                            class="block md:hidden bottom-20 right-20 z-99 bg-teal-100/50 text-black p-1 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor"
+                             class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div class="space-x-2 py-4 leading-normal tracking-tighter text-lg flex flex-row md:flex-row justify-center items-center">
                 <a href="#" class="hover:underline">
                     Contact
                 </a>
-                <span class="hidden md:block">|</span>
+                <span class="block">|</span>
                 <a href="#" class="hover:underline">Twitter</a>
             </div>
         </div>
